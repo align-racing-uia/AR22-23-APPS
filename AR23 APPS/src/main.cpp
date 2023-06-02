f73ad52655b65dd6af70973dddd2326205234710
hrrs01 <haavard.hn@gmail.com>
2023-06-02 17:59:02 +0200

    Newest version

--- AR23 APPS/src/main.cpp
+++ AR23 APPS/src/main.cpp
@@ -3,11 +3,16 @@
 #include <SPI.h>
 
 
-#define SDC A6
+#define SDC A5
 #define CAN_CS 10
 #define CAN_INT 2
 #define SENSOR1_CS 7
 #define SENSOR2_CS 8
+#define SENSOR1_MAX 3225
+#define SENSOR1_MIN 3015
+#define SENSOR2_MAX 1263
+#define SENSOR2_MIN 1053
+
 #define APPS_TRAVEL 210.0 // Degrees x 10^(-1)
 
 // ID for default broadcasting of R2D state, and pedal pressures.
@@ -19,6 +24,9 @@
 // ID for cascadia Relay and clear fault control.
 #define INV_RLY_ID 0x0C1
 #define INV_CLF_ID 0x0C1
+
+// ID of the dashboard brain, for spoofing.
+#define DB_ID 0x0E0
 
 // "Emergency" channels, to very quickly open SDC
 #define SDC_OPEN_ID1 0x0B0
@@ -51,22 +59,28 @@ bool precharge_button = false;
 bool shutdown_circuit = false;
 bool brakelight = false;
 bool buzzer = false;
+bool precharge_button_released = true;
 
-// Buzzer timestamp
+// Watchdog timer for R2D switch
+unsigned long r2d_timestamp = 0;
 
 // Deviation timestamp, to check if it lasts more than 100ms
-int deviation_timestamp = 0;
+unsigned long deviation_timestamp = 0;
 
 // The arduino should sample more often than it sends canbus messages / serial output
 // Therefore we need a timestamp for the last sent message.
-int rly_timestamp = 0;
-int broadcast_timestamp = 0;
-int command_timestamp = 0;
+unsigned long rly_timestamp = 0;
+unsigned long broadcast_timestamp = 0;
+
+unsigned long command_timestamp = 0;
 
 // CANBUS read variables
 long unsigned int rxId;
 unsigned char rxLen = 0;
 unsigned char rxBuf[8];
+
+// Tracking the current state of the inverter.
+int cascadia_state = 0;
 
 // Function to reset the apps sensor on startup
 // It expects a signal on SPI with 0x00 and 0x70 with a gap of 3 microseconds between.
@@ -90,6 +104,8 @@ uint32_t read_apps_sensor(int cs) {
   uint8_t val1_mask = 0x3F; 
 
   digitalWrite(cs, LOW);
+  // Needs a three microsecond delay after pulling CS low.
+  delayMicroseconds(3);
   SPI.beginTransaction(settings);
   val1 = SPI.transfer(0x00);
   delayMicroseconds(3);
@@ -124,6 +140,8 @@ void can_rx() {
   {
   case 0x0E0:
     
+    r2d_timestamp = millis(); // This is a check for the watchdog.
+
     ready_to_drive_switch = 0x1 & rxBuf[0];
     precharge_button = 0x2 & rxBuf[0];
   
@@ -138,8 +156,9 @@ void inverter_command(int throttle) {
 
   // First 2 bytes are used for torque.
   if (ready_to_drive){
-    commandData[0] = (throttle * MAX_TORQUE) & 0x00FF;
-    commandData[1] = ((throttle * MAX_TORQUE) >> 8) & 0xFF00;
+    int torque = (int) round(((double)throttle)/100.0 * MAX_TORQUE);
+    commandData[0] = torque & 0x00FF;
+    commandData[1] = (torque >> 8) & 0xFF;
   }
   
 
@@ -150,29 +169,27 @@ void inverter_command(int throttle) {
 
   // Byte 5, bit 1 is inverter enable, which it should be if we are ready to drive.
   commandData[5] |= ready_to_drive;
+
+  commandData[6] = MAX_TORQUE & 0x00FF;
+  commandData[7] = (MAX_TORQUE >> 8) & 0xFF;
   
   // Byte 6 and 7 is Commanded Torque limit, this is set in eeprom, so its no need to send new values.
 
-
+  // Serial.println("Sent inverter command!");
   can_tx(INV_CMD_ID, 8, commandData);
 }
 
 void inverter_clear_faults() {
   // For this one, just read the documentation. Cascadia CAN Protocol, Page 39.
+  // Byte order is: #0 #1 #2 #3 #4 #5 #6 #7
+  // Byte #0 and #1 is the parameter adress, in the following order #1#0. E.g. {0x14, 0x00} becomes 0x0014.
+  // Byte #2 is R/W commands. 0x0 for read, 0x01 for write.
+  // Byte #3, 6 and 7 is reserved and should be zero.
+  // Byte #4 and #5 is the data, which is explained in the documentation.
   byte clearFaultData[8] = {0x14, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00};
   can_tx(INV_CLF_ID, 8, clearFaultData);
 }
 
-void inverter_relay_states() {
-  // Initalizing default data according to cascadia documentation. Cascadia CAN Protocol, Page 40.
-  byte rlyData[8] = {0x01, 0x00, 0x01, 0x00, 0x00, 0x55, 0x00, 0x00};
-
-  rlyData[4] |= brakelight << 4;
-  rlyData[4] |= buzzer << 5;
-
-  can_tx(INV_RLY_ID, 8, rlyData);
-
-}
 
 
 
@@ -229,27 +246,32 @@ void loop() {
   shutdown_circuit = digitalRead(SDC);
 
   // Reading sensor input  
-  uint16_t sensor1 = read_apps_sensor(SENSOR1_CS);
-  uint16_t sensor2 = read_apps_sensor(SENSOR2_CS);
+  int raw_sensor1 = read_apps_sensor(SENSOR1_CS);
+  int raw_sensor2 = read_apps_sensor(SENSOR2_CS);
 
   
   // Handle incoming canbus messages
   can_rx();
 
-  // Calibrating the sensors as we go.
-  if(sensor1 < sensor1_min) {
-    sensor1_min = sensor1;
-  }
-
   // Actual pedal angle, sensor 1:
-  sensor1 -= sensor1_min;
-
-  if(sensor2 > sensor2_max) {
-    sensor2_max = sensor2;
+  // Setting the zero point of the sensor
+  if(raw_sensor1 < sensor1_min){
+    sensor1_min = raw_sensor1;
+  }
+  int sensor1 = raw_sensor1 - sensor1_min;
+  if(sensor1 < 0){
+    sensor1 = 0;
   }
 
   // Actual pedal angle, sensor 2:
-  sensor2 = abs(sensor2_max - sensor2);
+  // Setting the zero point of the sensor
+  if(raw_sensor2 > sensor2_max){
+    sensor2_max = raw_sensor2;
+  }
+  int sensor2 = sensor2_max - raw_sensor2;
+  if(sensor2 < 0){
+    sensor2 = 0;
+  }
 
   // Calculating percentage of full pedal travel
   int sg_percentage1 = sensor1 / APPS_TRAVEL * 100;
@@ -269,7 +291,7 @@ void loop() {
     if(deviation_timestamp == 0){
       deviation_timestamp = millis();
     }
-    if(millis() - deviation_timestamp > 50){
+    if(millis() - deviation_timestamp > 1000){
       deviation_error = true;
     }
     
@@ -277,23 +299,38 @@ void loop() {
     deviation_timestamp = 0;
   }
 
+  // If we havent heard anything from the dashboard in over a second, 
+  // we should have a failstate which assumes the switch is off.
+  if(millis() - r2d_timestamp > 2000){
+    ready_to_drive_switch = false;
+  }
+
   // Check if ready to drive should be enabled:
   if (!ready_to_drive){
-    if(throttle_signal < 5 || shutdown_circuit || ready_to_drive_switch){
-
+    if(throttle_signal < 5 && shutdown_circuit && ready_to_drive_switch){
+      inverter_clear_faults();
+      ready_to_drive = true;
     }
+  }
+
+  if (precharge_button && precharge_button_released){
+    precharge_button_released = false;
+  }
+  
+  if (!precharge_button){
+    precharge_button_released = true;
+  }
+
+  if (!shutdown_circuit || !ready_to_drive_switch){
+    ready_to_drive = false;
   }
 
   // Before sending messages to the inverter, we should make sure that the car is still in a good state:
   // TODO: Add more flags!
-  if (deviation_error) {
-    ready_to_drive = false;
-  }
+  // if (deviation_error) {
+  //   ready_to_drive = false;
+  // }
 
-  if(millis() - rly_timestamp > 50) {
-    inverter_relay_states();
-    rly_timestamp = millis();
-  } 
 
   if(millis() - command_timestamp > 10) {
     inverter_command(throttle_signal);
@@ -306,23 +343,35 @@ void loop() {
     // Prepare canbus frame:
     // Byte 1 is the pedal position, in a percentage
     // Byte 4 holds some states, with each bit meaning this: 0bABCDEFGH
-    // F - Ready to drive state
+    // E - Shutdown circuit
+    // F - Ready to drive switch
+    // G - Ready to drive state
     // H - Deviation Error
-    byte canFrame[4] = {0x0, 0x0, 0x0, 0x0};
+    byte canFrame[6] = {0x0, 0x0, 0x0, 0x00, 0x0, 0x0};
     canFrame[0] = throttle_signal;
-    canFrame[3] = ready_to_drive << 1;
-    canFrame[3] = deviation_error;
+
+    if(ready_to_drive){
+      canFrame[1] = (int) round((((double) throttle_signal / 100.0) * MAX_TORQUE) / 10.0);
+      
+    }
+
+    canFrame[4] = sensor1;
+    canFrame[5] = sensor2;
+
+    canFrame[3] = shutdown_circuit << 3;
+    canFrame[3] |= ready_to_drive_switch << 2;
+    canFrame[3] |= ready_to_drive << 1;
+    canFrame[3] |= deviation_error;
 
 
     // Send canbus frame:
-    can_tx(APPS_BROADCAST_ID, 4, canFrame);
+    can_tx(APPS_BROADCAST_ID, 6, canFrame);
 
     broadcast_timestamp = millis();
-    //Serial.println("Sensor 1: " + String(sensor1) + ", Sensor 2: " + String(sensor2));
+    Serial.println("Sensor 1: " + String(raw_sensor1) + ", Sensor 2: " + String(raw_sensor2));
+    Serial.println("Sensor 1%: " + String(sg_percentage1) + ", Sensor 2%: " + String(sg_percentage2));
     // Serial.println("Throttle %: " + String(canbus_signal));
   }
 
-  // The whole loop should have a minimum delay of 5ms.
-  delay(5);
 
 }
